0bcfa3f82142458031393734e1497cf370fc1e92
Nick Abell <darkforceyoda@gmail.com>
2023-03-25 12:55:17 -0500

    mobil responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { HiMenuAlt3 } from 'react-icons/hi';
-import { isScreenSmall } from '../utils/ScreenSize/screenSize';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     return (
-        isScreenSmall
-            ?
-            <div className='fixed w-full flex justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white'>
+        <>
+            <div className='fixed w-full flex justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white md:hidden'>
                 <h1>SMS</h1>
                 <HiMenuAlt3 onClick={() => setNav(!nav)} size={25} className='z-50 cursor-pointer' />
                 <div className={nav ? 'fixed w-full h-screen left-0 top-0 flex flex-col z-10 duration-500 ease-in-out' : 'absolute top-0 h-screen left-[-100%] ease-in-out duration-500'}>
@@ -19,8 +17,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-            :
-            <div className='fixed w-full flex justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white'>
+            <div className='fixed w-full justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white hidden md:flex'>
                 <h1>SMS</h1>
                 <ul className='flex w-full z-10 items-center justify-center gap-12'>
                     <li className='font-bold text-gray-400 text-3xl cursor-pointer hover:text-white'><a href='/'>Home</a></li>
@@ -29,6 +26,7 @@ const Navbar = () => {
                     <li className='font-bold text-gray-400 text-3xl cursor-pointer hover:text-white'><a href='/contact'>Contact</a></li>
                 </ul>
             </div>
+        </>
     )
 }
 
