11f719c75df47835de5de3f59f17bbae464eeb17
Nick Abell <darkforceyoda@gmail.com>
2023-03-29 17:20:19 -0500

    Footer responsive at xs screen size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ import { FiMail } from 'react-icons/fi';
 
 const Footer = () => {
     return (
-        <div className='w-full bg-black border-t-4 border-black flex flex-col items-center justify-center gap-12 px-16 min-h-[20vh] text-gray-400 py-12'>
-            <div className='flex items-center justify-center gap-8'>
+        <div className='w-full bg-black border-t-4 border-black flex flex-col items-center justify-center gap-24 sm:gap-12 px-16 min-h-[20vh] text-gray-400 py-20'>
+            <div className='flex flex-col sm:flex-row items-center justify-center gap-8'>
                 <a className='flex items-center justify-center gap-4 hover:text-white cursor-pointer'><BsTelephone size={25} /><h1> Call Us</h1></a>
                 <a className='flex items-center justify-center gap-4 hover:text-white cursor-pointer'><FiMail size={25} /><h1> Email Us</h1></a>
             </div>
-            <div className='flex items-center justify-center gap-8'>
+            <div className='flex flex-col sm:flex-row items-center justify-center gap-8'>
                 <a href='/about' className='hover:text-white'><h1>About Us</h1></a>
                 <a href='/services' className='hover:text-white'><h1>Our Services</h1></a>
                 <a href='/gallery' className='hover:text-white'><h1>Gallery</h1></a>
