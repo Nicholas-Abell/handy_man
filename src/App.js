ecf20d01cc13f090d8822c639ce09d4f3af91b43
Nick Abell <darkforceyoda@gmail.com>
2023-03-29 12:37:42 -0500

    netlify

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <BrowserRouter basename={'/handy_man'}>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/gallery' element={<Gallery />} />
