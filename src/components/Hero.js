5ce0e2d36000564269aa13c916b1f650413892fe
Nick Abell <darkforceyoda@gmail.com>
2023-03-29 19:11:09 -0500

    Hero Banner design change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,12 +16,9 @@ const Hero = () => {
             }}>
             <img src={before} className='top-0 left-0 w-full h-screen object-cover absolute z-10 ease-in opacity-100 duration-[3s]' style={fadeOut ? { opacity: '0', transitionDelay: '1s' } : { opacity: '100' }} />
             <div className='bg-black/10 absolute top-0 w-full h-screen'></div>
-            <div className='absolute top-0 w-full h-full flex flex-col justify-center items-center z-20'>
-                <div className='flex flex-col items-center justify-center gap-8 bg-red-900/70 p-4 sm:p-8 md:p-12 rounded-3xl min-w-[250px] md:min-w-[500px] lg::min-w-[600px]'>
-                    <h1 className='text-white text-lg sm:text-2xl md:text-3xl lg:text-7xl'>Transform Your Home</h1>
-                    <h1 className='text-white text-lg sm:text-xl md:text-2xl lg:text-5xl'>With SMS</h1>
-                    <button className='text-black bg-white hover:bg-gray-300 hover:shadow-xl ease-in duration-300'>Contact us Today</button>
-                </div>
+            <div className='absolute top-0 left-0 w-full h-full flex flex-col gap-4 justify-center items-center z-20'>
+                <h1 className='text-white text-3xl md:text-5xl lg:text-7xl'>Transform Your Home</h1>
+                <button className='text-white bg-red-800 border-black hover:bg-gray-400 hover:shadow-xl ease-in duration-300 max-w-[200px]'>Contact us Today</button>
             </div>
         </div>
     )
