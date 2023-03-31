8f2880886071a64a028ac011f8accc283945d1de
Nick Abell <darkforceyoda@gmail.com>
2023-03-31 00:30:54 -0500

    added svg

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../assets/logo/logo.svg';
 
 const AboutUs = () => {
     return (
-        <div className='text-center flex flex-col gap-4 justify-center items-center py-24 px-4 md:px-48 text-black bg-white'>
-            <h1 className='text-4xl text-red-800 font-bold'>About Us</h1>
-            <p className='text-2xl'>A small town handyman. Locally Owned and operated.</p>
+        <div className='text-center flex flex-col lg:flex-row gap-4 justify-center items-center py-24 px-4 md:px-48 text-black bg-white'>
+            <div className='flex flex-col justify-center items-center gap-4'>
+                <h1 className='text-4xl text-red-800 font-bold'>About Us</h1>
+                <p className='text-2xl'>A small town handyman. Locally Owned and operated. Serving Brownstown, Seymour, Columbus, and surrounding area.</p>
+            </div>
+            <Logo className='p-8 md:p-0' />
         </div>
     )
 }
