7ca7cfeae7fa47223070964510f4ac8b8a8816a3
Nick Abell <darkforceyoda@gmail.com>
2023-03-31 00:35:53 -0500

    added svg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { BsTelephone } from 'react-icons/bs';
-
+import { ReactComponent as Logo } from '../assets/logo/logo.svg';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -16,10 +16,11 @@ const Navbar = () => {
             <div className='fixed w-full flex justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white md:hidden'>
                 <div className='flex items-center justify-between w-[50%]'>
                     <a href="tel:8125251307"><BsTelephone size={25} className='cursor-pointer hover:text-green-400' /></a>
-                    <div className='relative text-center rounded-full bg-red-800 p-4 w-[20px] h-[20px] border border-black'>
+                    {/* <div className='relative text-center rounded-full bg-red-800 p-4 w-[20px] h-[20px] border border-black'>
                         <div className='rounded-full w-[5px] h-[5px] bg-red-500 absolute left-2 top-2 z-10'></div>
                         <h1 className='absolute bottom-[-1] left-0'>SMS</h1>
-                    </div>
+                    </div> */}
+                    <h1 className=' text-sm'>SMS</h1>
                 </div>
                 <HiMenuAlt3 onClick={() => setNav(!nav)} size={25} className='z-50 cursor-pointer' />
                 <div className={nav ? 'fixed w-full h-screen left-0 top-0 flex flex-col z-10 duration-500 ease-in-out' : 'absolute top-0 h-screen left-[-100%] ease-in-out duration-500'}>
@@ -33,8 +34,9 @@ const Navbar = () => {
             </div>
             <div className='fixed w-full justify-between items-center p-4 z-[999] bg-gradient-to-b from-black to-black/20 text-white hidden md:flex'>
                 <div className='relative text-center rounded-full bg-red-800 p-4 w-[20px] h-[20px] border border-black'>
-                    <div className='rounded-full w-[5px] h-[5px] bg-red-500 absolute left-2 top-2 z-10'></div>
-                    <h1 className='absolute bottom-[-1] left-0'>SMS</h1>
+                    {/* <div className='rounded-full w-[5px] h-[5px] bg-red-500 absolute left-2 top-2 z-10'></div>
+                    <h1 className='absolute bottom-[-1] left-0'>SMS</h1> */}
+                    <h1>SMS</h1>
                 </div>
                 <ul className='flex w-full z-10 items-center justify-center gap-12'>
                     <li className='font-bold text-gray-400 text-3xl cursor-pointer hover:text-white'><a href='/'>Home</a></li>
