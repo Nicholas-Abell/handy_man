2a851ae0b8db26e4d522d6b0934bbcf68c48f514
Nick <darkforceyoda@gmail.com>
2023-04-06 13:17:19 -0500

    h1 changes for seo keyword

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,8 +16,8 @@ const Hero = () => {
             }}>
             <img src={before} alt='before' className='top-0 left-0 w-full h-screen object-cover absolute z-10 ease-in opacity-100 duration-[3s]' style={fadeOut ? { opacity: '0', transitionDelay: '1s' } : { opacity: '100' }} />
             <div className='bg-black/10 absolute top-0 w-full h-screen'></div>
-            <div className='absolute top-0 left-0 w-full h-full flex flex-col gap-4 justify-center items-center z-20'>
-                <h1 className='text-white text-3xl md:text-5xl lg:text-6xl xl:text-7xl'>SMS <br className='sm:hidden' /> Construction & Remodeling</h1>
+            <div className='absolute top-0 left-0 w-full h-full flex flex-col gap-4 justify-center items-center z-20 text-center'>
+                <h1 className='text-white text-3xl md:text-5xl lg:text-6xl xl:text-7xl'>SMS Handyman <br /> Construction & Remodeling</h1>
                 <a href='/contact'><button className='text-white bg-red-800 border-black hover:bg-gray-400 hover:shadow-xl ease-in duration-300 max-w-[200px]'>Contact us Today</button></a>
             </div>
         </div>
