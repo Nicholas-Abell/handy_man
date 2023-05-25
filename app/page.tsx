0f2b1351740c97709bcc2d261f2d95835b66689b
Nick <darkforceyoda@gmail.com>
2023-05-25 14:35:21 -0500

    Card, CardHolder Comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Home/Hero";
+import AboutUs from "./components/Home/AboutUs";
+import CardHolder from "./components/Home/CardHolder";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
-      {/* <AboutUs />
+      <AboutUs />
       <CardHolder />
-      <IconBanner />
+      <AboutUs />
+      {/* <IconBanner />
       <ExteriorInterior /> */}
     </main>
   );
