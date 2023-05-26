eba25e8eec9de5233460b15a158f0979b92dc59b
Nick <darkforceyoda@gmail.com>
2023-05-26 15:42:47 -0500

    sitemap added

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -10,18 +10,18 @@ const Gallery = () => {
         <h1 className='text-5xl lg:text-8xl'>Gallery</h1>
       </div>
       <div className='w-full grid grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
-        <Image src={after} alt='interior design' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={bench} alt='bench' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={pathWay1} alt='pathway1' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={patio} alt='patio' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={shelving} alt='shelving' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={railing} alt='railing' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={cabinet} alt='cabinent' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={machinery} alt='machinery' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={pipe} alt='landscaping' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={awning2} alt='awning2' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
-        <Image src={deck2} alt='deck2' className='w-full h-auto object-cover rounded-lg row-span-1 col-span-1 border-4 border-black' />
-        <Image src={awning1} alt='awning1' className='w-full h-auto object-cover rounded-lg row-span-1 col-span-1 border-4 border-black' />
+        <Image sharp src={after} alt='interior design' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={bench} alt='bench' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={pathWay1} alt='pathway1' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={patio} alt='patio' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={shelving} alt='shelving' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={railing} alt='railing' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={cabinet} alt='cabinent' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={machinery} alt='machinery' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={pipe} alt='landscaping' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={awning2} alt='awning2' className='w-full h-auto object-cover rounded-lg border-4 border-black' />
+        <Image sharp src={deck2} alt='deck2' className='w-full h-auto object-cover rounded-lg row-span-1 col-span-1 border-4 border-black' />
+        <Image sharp src={awning1} alt='awning1' className='w-full h-auto object-cover rounded-lg row-span-1 col-span-1 border-4 border-black' />
       </div>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,16 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "SMS Construction and Remodeling",
   description:
     "SMS Construction and Remodeling. A Local Handyman serving Brownstown Indiana and surrounding counties.",
+  alternates: {
+    canonical: "https://smsconstructionandremodeling.com/",
+  },
   keywords: [
     "Handy Man",
     "Brownstown Indiana",
@@ -21,6 +23,8 @@ export const metadata: Metadata = {
     "SMS Construction and Remodeling",
   ],
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
