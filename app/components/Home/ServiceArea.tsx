621edb847729c6ad627a21e1612a0c454d586fd6
Nick <darkforceyoda@gmail.com>
2023-06-01 23:36:54 -0500

    section title size changes, text size for mobile

--- app/components/Home/ServiceArea.tsx
+++ app/components/Home/ServiceArea.tsx
@@ -5,13 +5,15 @@ import images from "@/app/assets/images/images";
 const ServiceArea: React.FC = () => {
   const [useLocalImages, setUseLocalImages] = useState(true);
   const { logo } = images;
-  
+
   return (
     <div className="flex flex-col items-center justify-center py-24 w-full">
-      <h2 className="text-4xl text-red-800 font-bold pb-4">Service Area</h2>
+      <h2 className="text-5xl md:text-7xl text-red-800 font-bold pb-4 md:pb-20">
+        Service Area
+      </h2>
       <div className="flex flex-col md:flex-row items-center gap-4 justify-center pt-4 w-full">
-        <div className="flex-col w-full justify-center items-center text-center">
-          <ul className="font-bold text-4xl">
+        <div className="flex-col w-full justify-center items-center text-center pb-4 md:p-0">
+          <ul className="font-bold text-2xl md:text-4xl">
             <li className="pt-4">
               <h2>JENNINGS</h2>
             </li>
@@ -25,7 +27,9 @@ const ServiceArea: React.FC = () => {
               <h2>MONROE</h2>
             </li>
             <li className="pt-4 md:pt-8">
-              <h2>SURROUNDING COUNTIES</h2>
+              <h2>
+                & SURROUNDING <br className="md:hidden" /> COUNTIES
+              </h2>
             </li>
           </ul>
         </div>
